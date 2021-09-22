8115fb890100591123b962001c2a1e3d074beaf2
jnghy <78326562+jnghy@users.noreply.github.com>
2021-09-22 12:16:41 +0800

    Tutorial 3

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        #region Pun Callbacks
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
